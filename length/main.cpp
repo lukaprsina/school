a95d53eba1b34b8bca566fc2adb2b2012df61f01
Luka Pršina <prsinaluka@gmail.com>
2021-09-24 21:55:26 +0200

    no templates, ostream overload

--- length/main.cpp
+++ length/main.cpp
@@ -18,34 +18,33 @@ const std::unordered_map<Unit, double> k_Map = {
     {Unit::Meters, 1000},
     {Unit::Inches, 25.4}};
 
-template <Unit DefaultUnit = Unit::Centimeters>
 class Length
 {
 public:
-    Length(double length)
-        : m_Length(length)
+    Length(double length, Unit unit)
+        : m_Length(length), m_Unit(unit)
     {
     }
 
-    double Convert(double length, Unit from, Unit to)
+    double Convert(double length, Unit from, Unit to) const
     {
         return length * k_Map.at(from) / k_Map.at(to);
     }
 
-    std::string ToMetric()
+    std::string ToMetric() const
     {
         double remaining = m_Length;
 
-        double meters = Convert(remaining, DefaultUnit, Unit::Meters);
-        remaining -= Convert(std::floor(meters), Unit::Meters, DefaultUnit);
+        double meters = Convert(remaining, m_Unit, Unit::Meters);
+        remaining -= Convert(std::floor(meters), Unit::Meters, m_Unit);
 
-        double decimeters = Convert(remaining, DefaultUnit, Unit::Decimeters);
-        remaining -= Convert(std::floor(decimeters), Unit::Decimeters, DefaultUnit);
+        double decimeters = Convert(remaining, m_Unit, Unit::Decimeters);
+        remaining -= Convert(std::floor(decimeters), Unit::Decimeters, m_Unit);
 
-        double centimeters = Convert(remaining, DefaultUnit, Unit::Centimeters);
-        remaining -= Convert(std::floor(centimeters), Unit::Centimeters, DefaultUnit);
+        double centimeters = Convert(remaining, m_Unit, Unit::Centimeters);
+        remaining -= Convert(std::floor(centimeters), Unit::Centimeters, m_Unit);
 
-        double milimeters = Convert(remaining, DefaultUnit, Unit::Milimeters);
+        double milimeters = Convert(remaining, m_Unit, Unit::Milimeters);
 
         return std::to_string(static_cast<int>(std::floor(meters))) + "m " +
                std::to_string(static_cast<int>(std::floor(decimeters))) + "dm " +
@@ -53,13 +52,18 @@ public:
                std::to_string(static_cast<int>(std::round(milimeters))) + "mm";
     }
 
+    friend std::ostream &operator<<(std::ostream &stream, const Length &length)
+    {
+        return stream << length.ToMetric();
+    }
+
 private:
-    double m_Length;
-    Unit m_Unit = DefaultUnit;
+    double m_Length{0.0f};
+    Unit m_Unit{Unit::Centimeters};
 };
 
 int main(int, char **)
 {
-    Length<Unit::Inches> length(1);
-    std::cout << length.ToMetric() << std::endl;
+    Length length(1, Unit::Inches);
+    std::cout << length << std::endl;
 }
